161e0d17a620bf1aa2520826a785b2beab771a31
Anton Soldatov <turbolet85@gmail.com>
2023-02-18 11:25:02 +0100

    upload to cloudinary

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -6,8 +6,10 @@ const {
    NotFoundError,
    BadRequestError,
 } = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
    if (!req.files) {
       throw new BadRequestError('No file uploaded');
    }
@@ -31,6 +33,20 @@ const uploadProductImage = async (req, res) => {
    return res
       .status(StatusCodes.OK)
       .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+   const result = await cloudinary.uploader.upload(
+      req.files.image.tempFilePath,
+      {
+         use_filename: true,
+         folder: 'file-upload',
+      }
+   );
+   fs.unlinkSync(req.files.image.tempFilePath);
+   return res
+      .status(StatusCodes.OK)
+      .json({ image: { src: result.secure_url } });
 };
 
 module.exports = { uploadProductImage };
